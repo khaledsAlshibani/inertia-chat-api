9b37fcb98ba68a6641b2ccd7f1ed1597459ae00f
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-05-29 08:14:46 +0300

    chore: setup spring boot environment

---
+++ src/main/java/com/inertia/chat/InertiaChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.inertia.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InertiaChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InertiaChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inertia/chat/InertiaChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inertia.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InertiaChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
