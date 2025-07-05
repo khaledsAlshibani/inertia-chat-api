636ddc34ce631c0b0a015745d239f63fc084df07
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-07-05 21:44:11 +0300

    fix(ws): ensure websocket CORS support match CORS of application.yml

--- src/main/java/com/inertia/chat/config/WebSocketConfig.java
+++ src/main/java/com/inertia/chat/config/WebSocketConfig.java
@@ -7,6 +7,8 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -14,6 +16,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSocketAuthChannelInterceptor webSocketAuthChannelInterceptor;
+
+    @Value("${spring.cors.allowed-origins}")
+    private List<String> allowedOrigins;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -25,7 +30,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOrigins(allowedOrigins.toArray(new String[0]))
                 .withSockJS()
                 .setDisconnectDelay(30 * 1000)
                 .setHeartbeatTime(25 * 1000);
